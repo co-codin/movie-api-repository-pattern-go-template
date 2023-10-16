dcecd7926f9bddf8cb10f3947c43b424bf9b249e
elijah <cuiyeqing960904@gmail.com>
2023-10-16 06:45:45 -0400

    user model built

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -42,6 +42,19 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	var payload struct {
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &payload)
+
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+
 	u := jwtUser{
 		ID:        1,
 		FirstName: "Admin",
