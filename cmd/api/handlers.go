7e06e8a03a4f5ed5cef640379b66f9b4c9c41cac
elijah <cuiyeqing960904@gmail.com>
2023-10-16 13:02:55 -0400

    auth middleware

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -139,3 +139,7 @@ func (app *application) logout(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, app.auth.GetExpiredRefreshCookie())
 	w.WriteHeader(http.StatusOK)
 }
+
+func (app *application) MovieCatalog(w http.ResponseWriter, r *http.Request) {
+	
+}
