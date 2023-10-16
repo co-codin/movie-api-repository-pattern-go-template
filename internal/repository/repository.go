5213c2f185d17692429d098b8cb50ac75002533b
elijah <cuiyeqing960904@gmail.com>
2023-10-16 16:31:47 -0400

    all genres endpoint

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,9 +8,10 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 	AllMovies() ([]*models.Movie, error)
-	
+
 	OneMovie(id int) (*models.Movie, error)
 	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
+	AllGenres() ([]*models.Genre, error)
 
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
